6bd256eb8019bec9ba15d874c10850da45ca65dd
LukaPodrug <lukapodra1@gmail.com>
2023-08-31 15:49:54 +0200

    Home view and controller initialization

---
+++ ToDoApp - UIKit/Controllers/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  ToDoApp - UIKit
+//
+//  Created by Luka Podrug on 31.08.2023..
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ToDoApp - UIKit/Views/HomeView.swift
@@ -0,0 +1,11 @@
+//
+//  HomeView.swift
+//  ToDoApp - UIKit
+//
+//  Created by Luka Podrug on 31.08.2023..
+//
+
+import UIKit
+
+class HomeView: UIView {
+}
